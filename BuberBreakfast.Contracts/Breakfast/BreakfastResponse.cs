cc63d32109625ed04deffc37e5d21473f1608934
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-06-15 17:58:48 +0600

    first commit

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,12 @@
+namespace BuberBreakfast.Contracts.BuberBreakfast;
+
+public record BreakfastResponse(
+    Guid Id,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDateTime,
+    List<string> Savory,
+    List<string> Sweet
+);
